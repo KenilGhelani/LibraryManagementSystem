63675421598a2d5929a03e0870dae4a3cc2ca538
Kenil Ghelani <kenilghelani2004@gmail.com>
2024-09-01 16:59:14 +0530

    Add constructor to initialize Book attributes

--- src/library/Book.java
+++ src/library/Book.java
@@ -6,4 +6,12 @@ public class Book {
     private String author;
     private int publicationYear;
     private boolean isAvailable;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true;
+    }
 }
