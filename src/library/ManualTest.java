6ac97c8adadaf23607fb6bccae297ffbc515497b
Kenil Ghelani <kenilghelani2004@gmail.com>
2024-09-01 23:31:51 +0530

    Add manual test for Book creation

--- src/library/ManualTest.java
+++ src/library/ManualTest.java
@@ -17,4 +17,10 @@ public class ManualTest {
         assertEqual("Test Book", book.getTitle(), "testBookCreation - Title");
         assertEqual(true, book.isAvailable(), "testBookCreation - Availability");
     }
+
+    public static void testBorrowBook() {
+        Book book = new Book("1234567890", "Test Book", "Test Author", 2021);
+        book.borrowBook();
+        assertEqual(false, book.isAvailable(), "testBorrowBook - Availability after borrow");
+    }
 }
