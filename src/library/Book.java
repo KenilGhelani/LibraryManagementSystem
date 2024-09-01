77e78324f5b85d27cafe7beea56c2dc18bd2c886
Kenil Ghelani <kenilghelani2004@gmail.com>
2024-09-01 16:56:17 +0530

    Create Book class with basic structure

---
+++ src/library/Book.java
@@ -0,0 +1,9 @@
+package library;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean isAvailable;
+}
