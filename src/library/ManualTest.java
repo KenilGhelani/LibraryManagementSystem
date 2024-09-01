6bd9e7bea53141201b7528ca69e4b497ed259092
Kenil Ghelani <kenilghelani2004@gmail.com>
2024-09-01 23:32:48 +0530

    Add manual test for handling borrowing of an unavailable Book

--- src/library/ManualTest.java
+++ src/library/ManualTest.java
@@ -23,4 +23,15 @@ public class ManualTest {
         book.borrowBook();
         assertEqual(false, book.isAvailable(), "testBorrowBook - Availability after borrow");
     }
+
+    public static void testBorrowBookWhenNotAvailable() {
+        try {
+            Book book = new Book("1234567890", "Test Book", "Test Author", 2021);
+            book.borrowBook();
+            book.borrowBook(); // This should throw an exception
+            System.out.println("testBorrowBookWhenNotAvailable - Failed");
+        } catch (IllegalStateException e) {
+            System.out.println("testBorrowBookWhenNotAvailable - Passed");
+        }
+    }
 }
